7d5788814b5d24de1c52205f14f7ef835e69a8cd
Mingran Zhou <zfrank1998@gmail.com>
2019-12-15 11:17:50 -0500

    add check for duplicate task

--- downloader-service/src/main/java/com/christopherzhz/downloader/service/DownloaderService.java
+++ downloader-service/src/main/java/com/christopherzhz/downloader/service/DownloaderService.java
@@ -2,21 +2,26 @@ package com.christopherzhz.downloader.service;
 
 import com.christopherzhz.downloader.downloaderImpl.Downloader;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.christopherzhz.downloader.utils.Constant.*;
 
 @RestController
 @RequestMapping("/downloader")
 public class DownloaderService {
+
+    private static ConcurrentHashMap<String, HashSet<String>> urlDestMap = new ConcurrentHashMap<>();
 
     private static class DownloaderResp extends HashMap {
         private DownloaderResp() {}
@@ -40,11 +45,22 @@ public class DownloaderService {
     public CompletableFuture<DownloaderResp> download(@RequestParam String url,
                                                       @RequestParam String destDir,
                                                       @RequestParam int nThreads) {
+        if (urlDestMap.containsKey(url) && urlDestMap.get(url).contains(destDir)) {
+            return fail("Task already exists!");
+        }
+
+        if (!urlDestMap.containsKey(url)) {
+            urlDestMap.put(url, new HashSet<>(Collections.singletonList(destDir)));
+        } else {
+            urlDestMap.get(url).add(destDir);
+        }
         try {
             Downloader downloader = new Downloader(url, destDir, nThreads);
             downloader.download();
         } catch (IOException | InterruptedException exp) {
             return fail(exp.getMessage());
+        } finally {
+            urlDestMap.get(url).remove(destDir);
         }
         return success();
     }
